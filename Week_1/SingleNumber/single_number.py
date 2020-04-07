8186ddf3552b33fd8d0432470462b798c42f466b
Shubham Wagh <shubhamwagh48@gmail.com>
2020-04-07 11:10:24 +0100

    Update single_number.py

--- Week_1/SingleNumber/single_number.py
+++ Week_1/SingleNumber/single_number.py
@@ -1,6 +1,8 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:      
         res = nums[0]
-        for i in range(1,len(nums)): 
+        for i in range(1,len(nums)):
+            # XOR of a number with itself gives 0
+            # XOR of a number with 0 is the number itself
             res = res ^ nums[i] 
         return res
